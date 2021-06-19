804b09f8b89135e8eda2221b9bac7203217eab8f
LiYechao <liyechao@yechao.xyz>
2021-06-19 16:52:09 +0800

    fix: Safari heading input chinese error

--- editor_src/src/Editor/nodes/Heading.ts
+++ editor_src/src/Editor/nodes/Heading.ts
@@ -14,14 +14,14 @@ export default class Heading extends Node {
       group: 'block',
       defining: true,
       parseDOM: [
-        { tag: 'h1', attrs: { level: 1 } },
-        { tag: 'h2', attrs: { level: 2 } },
-        { tag: 'h3', attrs: { level: 3 } },
-        { tag: 'h4', attrs: { level: 4 } },
-        { tag: 'h5', attrs: { level: 5 } },
-        { tag: 'h6', attrs: { level: 6 } },
+        { tag: 'h1', attrs: { level: 1 }, contentElement: 'span' },
+        { tag: 'h2', attrs: { level: 2 }, contentElement: 'span' },
+        { tag: 'h3', attrs: { level: 3 }, contentElement: 'span' },
+        { tag: 'h4', attrs: { level: 4 }, contentElement: 'span' },
+        { tag: 'h5', attrs: { level: 5 }, contentElement: 'span' },
+        { tag: 'h6', attrs: { level: 6 }, contentElement: 'span' },
       ],
-      toDOM: node => ['h' + node.attrs.level, 0],
+      toDOM: node => ['h' + node.attrs.level, ['span', 0]],
     }
   }
 
